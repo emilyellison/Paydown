9d750869f255683494975c6b416fa544d8e577c8
Emily Ellison <emilyellison986@gmail.com>
2012-06-10 13:55:06 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Paydown::Application.config.secret_token = 'fc8a76df682b0acea8da324e7771caf0c18fa56208be4c11ca9f6bf10daf3e6afc2e0f93d9000467056f5afd7778da5f6b77bc90961391049b7bf1012212852f'
